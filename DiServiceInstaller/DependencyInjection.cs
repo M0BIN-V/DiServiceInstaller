10588d8aec3f52a944d03b3f17e304362e2de4cf
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-08-08 03:46:29 +0330

    Add project files.

---
+++ DiServiceInstaller/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace DiServiceInstaller;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection InstallServices(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        params Assembly[] assemblies)
+    {
+        var installers = assemblies
+            .SelectMany(a => a.DefinedTypes)
+            .Where(a => !a.IsAbstract && !a.IsInterface && a
+                .IsAssignableFrom(typeof(IServiceInstaller)))
+            .Select(Activator.CreateInstance)
+            .Cast<IServiceInstaller>();
+
+        foreach (var installer in installers)
+        {
+            installer.Install(services, configuration);
+        }
+
+        return services;
+    }
+}
