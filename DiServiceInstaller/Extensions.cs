d20664ae68db2494404e45f87e29ad72b0368100
Mobin Valikhani <mobinvalikhani@gmail.com>
2025-01-16 20:55:48 +0330

    chore: add main.yml

--- DiServiceInstaller/Extensions.cs
+++ DiServiceInstaller/Extensions.cs
@@ -1,11 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
+﻿using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 
 namespace DiServiceInstaller;
 
-public static class DependencyInjection
+public static class Extensions
 {
     public static IApplicationBuilder InstallServices(
         this IApplicationBuilder applicationBuilder,
@@ -13,15 +11,12 @@ public static class DependencyInjection
     {
         var installers = assemblies
             .SelectMany(a => a.DefinedTypes)
-            .Where(a => !a.IsAbstract && !a.IsInterface && a
+            .Where(a => a is { IsAbstract: false, IsInterface: false } && a
                 .IsAssignableTo(typeof(IServiceInstaller)))
             .Select(Activator.CreateInstance)
             .Cast<IServiceInstaller>();
 
-        foreach (var installer in installers)
-        {
-            installer.Install(applicationBuilder);
-        }
+        foreach (var installer in installers) installer.Install(applicationBuilder);
 
         return applicationBuilder;
     }
